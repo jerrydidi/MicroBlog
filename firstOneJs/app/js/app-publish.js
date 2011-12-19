e6a5f1d9cc058d7d6718a27ee169a6eec33abdfc
Lijian <lijian.bao@live.com>
2011-12-19 18:49:41 +0800

    try get but failed

--- firstOneJs/app/js/app-publish.js
+++ firstOneJs/app/js/app-publish.js
@@ -14,7 +14,7 @@
 				   //window.location.reload();
 				   $.ajax({url:"https://api.weibo.com/2/statuses/update.json",
 						   data: {access_token: token, status:encodeURIComponent(wb_post.text)},
-						   method: "POST",
+						   method: "GET",
 						   dataType: "jsonp",
 						   crossDomain: "true",
 						   jsonpCallback: "jQueryJSONResultFun",
